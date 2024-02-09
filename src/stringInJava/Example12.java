df6019419bff62543d76bd150b02fb5b096095a0
VIDYA <rautvidya2000@gmail.com>
2024-02-09 13:25:38 +0530

    Try to push code in github

--- src/stringInJava/Example12.java
+++ src/stringInJava/Example12.java
@@ -5,7 +5,7 @@ public class Example12 {
     public static void main(String[]args){
         Scanner odd = new Scanner(System.in);
         System.out.println("Enter odd Number");
-
+        // Added some code
 
     }
 }
